155a25be79137314229a41e49c40a5cced21c8f6
alejo <alejoponin2015@hotmail.com>
2022-04-21 22:23:23 -0300

    Listo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,7 +181,7 @@ function esPrimo(numero) {
       return false;
     }
   }
-    return true;
+  return true;
 }
 
 function esVerdadero(valor){
@@ -221,7 +221,7 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  var i = 0
+  let i = 0;
   do{
     i = i + 1;
     numero = numero + 5;
